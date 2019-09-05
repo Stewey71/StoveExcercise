b85fd99972f512050934fac18acd8838a0246e95
Stewey71 <zane.deaton@yahoo.com>
2019-09-05 10:24:52 -0600

    Created Setting Enum, and intialized Burner class vars, and created plusButton and minus button function.

--- src/Burner.java
+++ src/Burner.java
@@ -1,4 +1,54 @@
 
 public class Burner {
+	Burner(){
+		myTemperature = Temperature.COLD;
+		mySetting = Setting.OFF;
+	}
+	public enum Temperature{
+		BLAZING, HOT, WARM, COLD
+	}
+
+	private Temperature myTemperature;
+	private Setting mySetting;
+	private int myTimer;
+	public static final int TIME_DURATION = 2;
+
+	void plusButton() {
+		myTimer = TIME_DURATION;
+		switch(mySetting) {
+		case OFF:
+			mySetting = Setting.LOW;
+			break;
+		case LOW:
+			mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			mySetting = Setting.HIGH;
+			break;
+		case HIGH:
+			break;
+		}
+	}
+
+	void minusButton() {
+		myTimer = TIME_DURATION;
+		switch(mySetting) {
+		case OFF:
+			break;
+		case LOW:
+			mySetting = Setting.OFF;
+			break;
+		case MEDIUM:
+			mySetting = Setting.LOW;
+			break;
+		case HIGH:
+			mySetting = Setting.MEDIUM;
+			break;
+		}
+
+	}
+
+
+
 
 }
--- src/Setting.java
+++ src/Setting.java
@@ -1,4 +1,14 @@
 
 public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	private String settingValue;
+	
+	Setting(String myValue){
+		settingValue = myValue;
+	}
+	
+	public String toString() {
+		return settingValue;
+	}
 
 }
