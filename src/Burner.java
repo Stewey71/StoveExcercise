75c0ec054f254072c78dded1b3cdc4446277d54a
Jerry Birnbaum <jerrywbirnbaum@mines.edu>
2019-09-11 15:13:52 -0600

    Implemented update Temperature

--- src/Burner.java
+++ src/Burner.java
@@ -3,6 +3,8 @@ public class Burner {
 	Burner(){
 		myTemperature = Temperature.COLD;
 		mySetting = Setting.OFF;
+		tempChange = false;
+		myTimer = 0;
 	}
 	public enum Temperature{
 		BLAZING, HOT, WARM, COLD
@@ -11,6 +13,7 @@ public class Burner {
 	private Temperature myTemperature;
 	private Setting mySetting;
 	private int myTimer;
+	private boolean tempChange;
 	public static final int TIME_DURATION = 2;
 
 	void plusButton() {
@@ -28,6 +31,7 @@ public class Burner {
 		case HIGH:
 			break;
 		}
+		tempChange = true;
 	}
 
 	void minusButton() {
@@ -45,7 +49,76 @@ public class Burner {
 			mySetting = Setting.MEDIUM;
 			break;
 		}
+		tempChange = true;
 
+	}
+	
+	void updateTemperature() {
+		--myTimer;
+		if(myTimer == 0) {
+			switch(myTemperature) {
+			case COLD:
+				switch(mySetting) {
+				case OFF:
+					break;
+				case LOW:
+					myTemperature = myTemperature.WARM;
+					break;
+				case MEDIUM:
+					myTemperature = myTemperature.WARM;
+					break;
+				case HIGH:
+					myTemperature = myTemperature.WARM;
+					break;
+				}
+				break;
+			case WARM:
+				switch(mySetting) {
+				case OFF:
+					myTemperature = myTemperature.COLD;
+					break;
+				case LOW:
+					break;
+				case MEDIUM:
+					myTemperature = myTemperature.HOT;
+					break;
+				case HIGH:
+					myTemperature = myTemperature.HOT;
+					break;
+				}
+				break;
+			case HOT:
+				switch(mySetting) {
+				case OFF:
+					myTemperature = myTemperature.WARM;
+					break;
+				case LOW:
+					myTemperature = myTemperature.WARM;
+					break;
+				case MEDIUM:
+					break;
+				case HIGH:
+					myTemperature = myTemperature.BLAZING;
+					break;
+				}
+				break;
+			case BLAZING:
+				switch(mySetting) {
+				case OFF:
+					myTemperature = myTemperature.HOT;
+					break;
+				case LOW:
+					myTemperature = myTemperature.HOT;
+					break;
+				case MEDIUM:
+					myTemperature = myTemperature.HOT;
+					break;
+				case HIGH:
+					break;
+				}
+				break;
+			}
+		}
 	}
 
 
